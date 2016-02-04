69a71bef1933030f1161bae12361e6d5af77c6ad
Katirene <kat.traxler@gmail.com>
2016-02-04 12:49:19 -0600

    work through Thursday morning

--- script.js
+++ script.js
@@ -1,31 +1,68 @@
-function Fruit (name, price){
+function Fruit (name, price, quantity){
 	this.name = name;
 	this.price = price;
+	this.quantity = quantity;
 }
 
-var apples = new Fruit ('apples', randomNumber(50, 999));
-var oranges = new Fruit ('oranges', randomNumber(50, 999));
-var bananas = new Fruit ('bananas', randomNumber(50, 999));
-var grapes = new Fruit ('grapes', randomNumber(50, 999));
+var apples = new Fruit ('apples', randomNumber(50, 999), 0);
+var oranges = new Fruit ('oranges', randomNumber(50, 999), 0);
+var bananas = new Fruit ('bananas', randomNumber(50, 999), 0);
+var grapes = new Fruit ('grapes', randomNumber(50, 999), 0);
 
 
 var fruitArray = [apples, oranges, bananas, grapes];
 
+var userCash = 10000;
+
+
+// var $el = $('meowCats').children().last();
+// 	$el.data('catvalue', counter);
+
 console.log(fruitArray);
 
 $(document).ready(function(){
-	$('.apples').append(fruitArray[0].price / 100);
-	$('.oranges').append(fruitArray[1].price / 100);
-	$('.bananas').append(fruitArray[2].price / 100);
-	$('.grapes').append(fruitArray[3].price / 100);
+	$('p .applesPrice').text(fruitArray[0].price / 100);
+	var $el = $('#apples').children().find('.applesPrice');
+ 	$el.data('price-apples', fruitArray[0].price / 100);
 
-	// var setInitial = setInterval( function() {
+ 	$('p .orangesPrice').text(fruitArray[1].price / 100);
+	var $el = $('#oranges').children().find('.orangesPrice');
+ 	$el.data('price-oranges', fruitArray[1].price / 100);
 
-	// }
+ 	$('p .bananasPrice').text(fruitArray[2].price / 100);
+	var $el = $('#bananas').children().find('.bananasPrice');
+ 	$el.data('price-bananas', fruitArray[2].price / 100);
 
+ 	$('p .grapesPrice').text(fruitArray[3].price / 100);
+	var $el = $('#grapes').children().find('.grapesPrice');
+ 	$el.data('price-grapes', fruitArray[3].price / 100);
+
+	$('p .user').text(userCash / 100);
+	var $el = $('#user').children().find('.user');
+ 	$el.data('user-money', userCash/100);
+
+
+	console.log($('.applesPrice').data('price-apples'));
+
+	$('.apples').on('click', function() {
+		buyFruit(apples);
+		$('.user').text(userCash/100);
+	});
+
+
+	// 	var newApplesPrice = $('.applesPrice').data('price-apples');
+	// 	buyFruit(applePrice);
+	// 	console.log(newApplesPrice);
+	// });
 
 });
 
+function buyFruit(fruit){
+	userCash -= fruit.price;
+	fruit.quantity++;
+	console.log(fruit.quantity);
+	console.log(userCash);
+}
 
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
@@ -43,11 +80,12 @@ function priceAdjust() {
 	 } else if (fruitArray[i].price < 50) {
 	 	fruitArray[i].price = 50;
 	 }
+	 $('p' + array[i].name).text(fruitArray[3].price / 100);fruitArray[i].price
 	console.log(fruitArray[i].price);
 	}
 }
 
-console.log(intervalID);
+
 // // randomNumber(0.50, 9.99);
 // // console.log(randomNumber(0.50, 9.99));
 // // });
